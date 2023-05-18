630fff663e381591635a52f84dd3ed70e9497b1e
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-05-18 18:32:50 -0400

    added config files so that this project can be opened in visual studio

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
     glViewport(0, 0, width, height);
