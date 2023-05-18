308cc17c65019ee0ba9bd813dc8eb7481b37d5dc
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-05-18 17:48:08 -0400

    added color

--- main.cpp
+++ main.cpp
@@ -38,6 +38,10 @@ int main()
 
     //render loop
     while(!glfwWindowShouldClose(window)){
+        //testing a color
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
