c5a783abad37e3b9b5eb85bb34f9aeb5bc440764
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-05-18 17:45:44 -0400

    rendering loop works!

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	g++ main.cpp -I C:/glad/include -I C:/glfw-3.3.8/glfw-3.3.8/include -L C:/glfw-3.3.8/glfw-3.3.8/build/src -lglfw3 -lopengl32 -lgdi32 -o main.exe
+	g++ main.cpp C:/glad/src/glad.c -I C:/glad/include -I C:/glfw-3.3.8/glfw-3.3.8/include -L C:/glfw-3.3.8/glfw-3.3.8/build/src -lglfw3 -lopengl32 -lgdi32 -o main.exe
 run:
 	./main
 clean:
--- main.cpp
+++ main.cpp
@@ -6,13 +6,45 @@
 
 using namespace std;
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+    if (!glfwInit()){
+        printf("failed to initialize glfw context!\n");
     }
-    cout << endl;
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+    // for mac osx glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL){
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
+
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
+    //render loop
+    while(!glfwWindowShouldClose(window)){
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    // cleanup
+    glfwTerminate();
+    return 0;
 }
+
+  
