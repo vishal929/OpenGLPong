6067425167cd389daaac6175435c81b618a1e5be
vishal929 <48373882+vishal929@users.noreply.github.com>
2023-05-26 13:18:08 -0400

    start main menu UI

---
+++ Views/MainMenu.cpp
@@ -0,0 +1,5 @@
+// holds logic for the UI for the Main Menu of our Pong Game
+// ultimately has 2 options: single player and multiplayer
+// --> we should have a title displaying that this is pong and we should have 2 buttons for the options
+
+
